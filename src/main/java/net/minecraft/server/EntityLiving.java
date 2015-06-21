c55fd8b2efb6c346f0f9411643f27d3382240c61
Jedediah Smith <jedediah@silencegreys.com>
2015-06-21 15:07:20 -0400

    Custom replacement for eaten items

--- src/main/java/net/minecraft/server/EntityLiving.java
+++ src/main/java/net/minecraft/server/EntityLiving.java
@@ -2817,12 +2817,13 @@ public abstract class EntityLiving extends Entity {
 
     protected void q() {
         if (!this.activeItem.isEmpty() && this.isHandRaised()) {
+            PlayerItemConsumeEvent event = null; // Paper
             this.b(this.activeItem, 16);
             // CraftBukkit start - fire PlayerItemConsumeEvent
             ItemStack itemstack;
             if (this instanceof EntityPlayer) {
                 org.bukkit.inventory.ItemStack craftItem = CraftItemStack.asBukkitCopy(this.activeItem);
-                PlayerItemConsumeEvent event = new PlayerItemConsumeEvent((Player) this.getBukkitEntity(), craftItem);
+                event = new PlayerItemConsumeEvent((Player) this.getBukkitEntity(), craftItem); // Paper
                 world.getServer().getPluginManager().callEvent(event);
 
                 if (event.isCancelled()) {
@@ -2837,9 +2838,20 @@ public abstract class EntityLiving extends Entity {
                 itemstack = this.activeItem.a(this.world, this);
             }
 
+            // Paper start - save the default replacement item and change it if necessary
+            final ItemStack defaultReplacement = itemstack;
+            if (event != null && event.getReplacement() != null) {
+                itemstack = CraftItemStack.asNMSCopy(event.getReplacement());
+            }
+            // Paper end
             this.a(this.getRaisedHand(), itemstack);
             // CraftBukkit end
             this.dp();
+            // Paper start - if the replacement is anything but the default, update the client inventory
+            if (this instanceof EntityPlayer && !com.google.common.base.Objects.equal(defaultReplacement, itemstack)) {
+                ((EntityPlayer) this).getBukkitEntity().updateInventory();
+            }
+            // Paper end
         }
 
     }
