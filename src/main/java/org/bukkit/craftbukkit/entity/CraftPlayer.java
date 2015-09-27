5a0abfa04a8e5b30b03463fa86fe8aab4c4e1ea1
Aikar <aikar@aikar.co>
2015-09-27 01:18:02 -0400

    handle NaN health/absorb values and repair bad data

--- src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -1565,6 +1565,7 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
     }
 
     public void setRealHealth(double health) {
+        if (Double.isNaN(health)) {return;} // Paper
         this.health = health;
     }
 
