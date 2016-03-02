934cbd72e5d5ee1a7fc17d3debc5f077f4126457
Sudzzy <originmc@outlook.com>
2016-03-02 15:03:53 -0600

    Configurable mob spawner tick rate

--- src/main/java/com/destroystokyo/paper/PaperWorldConfig.java
+++ src/main/java/com/destroystokyo/paper/PaperWorldConfig.java
@@ -166,4 +166,9 @@ public class PaperWorldConfig {
     private void disableIceAndSnow(){
         disableIceAndSnow = getBoolean("disable-ice-and-snow", false);
     }
+
+    public int mobSpawnerTickRate;
+    private void mobSpawnerTickRate() {
+        mobSpawnerTickRate = getInt("mob-spawner-tick-rate", 1);
+    }
 }
