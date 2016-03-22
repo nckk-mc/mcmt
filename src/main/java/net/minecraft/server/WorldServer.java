fc0506e05bbcdc5f51ccb38cf2dfa4c3e338fd58
Zach Brown <zach.brown@destroystokyo.com>
2016-03-22 12:04:28 -0500

    Configurable spawn chances for skeleton horses

--- src/main/java/net/minecraft/server/WorldServer.java
+++ src/main/java/net/minecraft/server/WorldServer.java
@@ -388,7 +388,7 @@ public class WorldServer extends World {
             blockposition = this.a(this.a(j, 0, k, 15));
             if (this.isRainingAt(blockposition)) {
                 DifficultyDamageScaler difficultydamagescaler = this.getDamageScaler(blockposition);
-                boolean flag1 = this.getGameRules().getBoolean("doMobSpawning") && this.random.nextDouble() < (double) difficultydamagescaler.b() * 0.01D;
+                boolean flag1 = this.getGameRules().getBoolean("doMobSpawning") && this.random.nextDouble() < (double) difficultydamagescaler.b() * paperConfig.skeleHorseSpawnChance; // Paper;
 
                 if (flag1) {
                     EntityHorseSkeleton entityhorseskeleton = (EntityHorseSkeleton) EntityTypes.SKELETON_HORSE.a((World) this);
