0fdab605afb2f86b8afd8cc56c8a98cc2bf8d26e
md_5 <git@md-5.net>
2016-07-30 11:29:14 +1000

    Branchless NibbleArray

--- src/main/java/net/minecraft/server/NibbleArray.java
+++ src/main/java/net/minecraft/server/NibbleArray.java
@@ -32,29 +32,27 @@ public class NibbleArray {
         return j << 8 | k << 4 | i;
     }
 
-    private int a(int i) {
+    public int a(int i) { // PAIL: private -> public
         if (this.a == null) {
             return 0;
         } else {
             int j = this.c(i);
 
-            return this.b(i) ? this.a[j] & 15 : this.a[j] >> 4 & 15;
+            return this.a[j] >> ((i & 1) << 2) & 15; // Spigot
         }
     }
 
-    private void a(int i, int j) {
+    public void a(int i, int j) { // PAIL: private -> public
         if (this.a == null) {
             this.a = new byte[2048];
         }
 
         int k = this.c(i);
 
-        if (this.b(i)) {
-            this.a[k] = (byte) (this.a[k] & 240 | j & 15);
-        } else {
-            this.a[k] = (byte) (this.a[k] & 15 | (j & 15) << 4);
-        }
-
+        // Spigot start
+        int shift = (i & 1) << 2;
+        this.a[k] = (byte) (this.a[k] & ~(15 << shift) | (j & 15) << shift);
+        // Spigot end
     }
 
     private boolean b(int i) {
