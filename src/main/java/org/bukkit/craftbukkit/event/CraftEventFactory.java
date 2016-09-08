54915bb0080267eea760a34c11efd12dc20dd8cb
AlphaBlend <whizkid3000@hotmail.com>
2016-09-08 08:48:33 -0700

    Add source to PlayerExpChangeEvent

--- src/main/java/org/bukkit/craftbukkit/event/CraftEventFactory.java
+++ src/main/java/org/bukkit/craftbukkit/event/CraftEventFactory.java
@@ -106,6 +106,7 @@ import org.bukkit.entity.ThrownPotion;
 import org.bukkit.entity.Vehicle;
 import org.bukkit.entity.Villager;
 import org.bukkit.entity.Villager.Profession;
+import org.bukkit.entity.ExperienceOrb; // Paper
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Result;
@@ -972,6 +973,17 @@ public class CraftEventFactory {
         Bukkit.getPluginManager().callEvent(event);
         return event;
     }
+
+    // Paper start - Add orb
+    public static PlayerExpChangeEvent callPlayerExpChangeEvent(EntityHuman entity, EntityExperienceOrb entityOrb) {
+        Player player = (Player) entity.getBukkitEntity();
+        ExperienceOrb source = (ExperienceOrb) entityOrb.getBukkitEntity();
+        int expAmount = source.getExperience();
+        PlayerExpChangeEvent event = new PlayerExpChangeEvent(player, source, expAmount);
+        Bukkit.getPluginManager().callEvent(event);
+        return event;
+    }
+    // Paper end
 
     public static boolean handleBlockGrowEvent(World world, BlockPosition pos, IBlockData block) {
         return handleBlockGrowEvent(world, pos, block, 3);
