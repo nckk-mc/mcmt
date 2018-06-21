39897f024dfd2ab516eebb99d9dd154e9bdadd15
Aikar <aikar@aikar.co>
2018-06-20 23:17:24 -0400

    Expand Explosions API
    Add Entity as a Source capability, and add more API choices, and on Location.

--- src/main/java/org/bukkit/craftbukkit/CraftWorld.java
+++ src/main/java/org/bukkit/craftbukkit/CraftWorld.java
@@ -753,6 +753,11 @@ public class CraftWorld implements World {
     public boolean createExplosion(double x, double y, double z, float power, boolean setFire, boolean breakBlocks) {
         return !world.createExplosion(null, x, y, z, power, setFire, breakBlocks ? Explosion.Effect.BREAK : Explosion.Effect.NONE).wasCanceled;
     }
+    // Paper start
+    public boolean createExplosion(Entity source, Location loc, float power, boolean setFire, boolean breakBlocks) {
+        return !world.createExplosion(source != null ? ((org.bukkit.craftbukkit.entity.CraftEntity) source).getHandle() : null, loc.getX(), loc.getY(), loc.getZ(), power, setFire, breakBlocks ? Explosion.Effect.BREAK : Explosion.Effect.NONE).wasCanceled;
+    }
+    // Paper end
 
     @Override
     public boolean createExplosion(Location loc, float power) {
