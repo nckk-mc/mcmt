pvginkel/Tweakkit-Server

Add Late Bind Option
Add late-bind config option to delay binding until loading is done.

src/main/java/net/minecraft/server/DedicatedServer.java
@@ -119,13 +119,15 @@ public class DedicatedServer extends MinecraftServer implements IMinecraftServer
         this.a(MinecraftEncryption.b());
         h.info("Starting Minecraft server on " + (this.getServerIp().length() == 0 ? "*" : this.getServerIp()) + ":" + this.K());
 
-        try {
-            this.ah().a(inetaddress, this.K());
-        } catch (Throwable ioexception) { // CraftBukkit - IOException -> Throwable
-            h.warn("**** FAILED TO BIND TO PORT!");
-            h.warn("The exception was: {}", new Object[] { ioexception.toString()});
-            h.warn("Perhaps a server is already running on that port?");
-            return false;
+        if (!org.spigotmc.SpigotConfig.lateBind) {
+            try {
+                this.ah().a(inetaddress, this.K());
+            } catch (Throwable ioexception) { // CraftBukkit - IOException -> Throwable
+                h.warn("**** FAILED TO BIND TO PORT!");
+                h.warn("The exception was: {}", new Object[] { ioexception.toString()});
+                h.warn("Perhaps a server is already running on that port?");
+                return false;
+            }
         }
 
         // Spigot Start - Move DedicatedPlayerList up and bring plugin loading from CraftServer to here
@@ -186,6 +188,18 @@ public class DedicatedServer extends MinecraftServer implements IMinecraftServer
         String s3 = String.format("%.3fs", new Object[] { Double.valueOf((double) i1 / 1.0E9D)});
 
         h.info("Done (" + s3 + ")! For help, type \"help\" or \"?\"");
+
+        if (org.spigotmc.SpigotConfig.lateBind) {
+            try {
+                this.ah().a(inetaddress, this.K());
+            } catch (Throwable ioexception) { // CraftBukkit - IOException -> Throwable
+                h.warn("**** FAILED TO BIND TO PORT!");
+                h.warn("The exception was: {}", new Object[] { ioexception.toString()});
+                h.warn("Perhaps a server is already running on that port?");
+                return false;
+            }
+        }
+
         if (this.propertyManager.getBoolean("enable-query", false)) {
             h.info("Starting GS4 status listener");
             this.j = new RemoteStatusListener(this);

nckk-mc/mcmt

src/main/java/net/minecraft/server/DedicatedServer.java
@@ -178,6 +178,7 @@ public class DedicatedServer extends MinecraftServer implements IMinecraftServer
         this.a(MinecraftEncryption.b());
         DedicatedServer.LOGGER.info("Starting Minecraft server on {}:{}", this.getServerIp().isEmpty() ? "*" : this.getServerIp(), this.getPort());
 
+    if (!org.spigotmc.SpigotConfig.lateBind) {
         try {
             this.getServerConnection().a(inetaddress, this.getPort());
         } catch (IOException ioexception) {
@@ -186,6 +187,7 @@ public class DedicatedServer extends MinecraftServer implements IMinecraftServer
             DedicatedServer.LOGGER.warn("Perhaps a server is already running on that port?");
             return false;
         }
+    }
 
         // CraftBukkit start
         // this.a((PlayerList) (new DedicatedPlayerList(this))); // Spigot - moved up
@@ -267,6 +269,17 @@ public class DedicatedServer extends MinecraftServer implements IMinecraftServer
                 this.remoteControlListener.a();
                 this.remoteConsole = new org.bukkit.craftbukkit.command.CraftRemoteConsoleCommandSender(this.remoteControlCommandListener); // CraftBukkit
             }
+
+    if (org.spigotmc.SpigotConfig.lateBind) {
+        try {
+            this.getServerConnection().a(inetaddress, this.getPort());
+        } catch (IOException ioexception) {
+            DedicatedServer.LOGGER.warn("**** FAILED TO BIND TO PORT!");
+            DedicatedServer.LOGGER.warn("The exception was: {}", ioexception.toString());
+            DedicatedServer.LOGGER.warn("Perhaps a server is already running on that port?");
+            return false;
+        }
+    }
 
             if (false && this.getMaxTickTime() > 0L) {  // Spigot - disable
                 Thread thread1 = new Thread(new ThreadWatchdog(this));