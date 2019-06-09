pvginkel/Tweakkit-Server

Fix Null Tile Entity Worlds

src/main/java/net/minecraft/server/World.java
@@ -1956,6 +1956,7 @@ public abstract class World implements IBlockAccess {
                     }
                 }
 
+                tileentity.a(this); // Spigot - No null worlds
                 this.a.add(tileentity);
             } else {
                 this.tileEntityList.add(tileentity);

nckk-mc/mcmt

src/main/java/net/minecraft/server/World.java
@@ -949,6 +949,7 @@ public abstract class World implements IIBlockAccess, GeneratorAccess, AutoClose
                         }
                     }
 
+                    tileentity.setWorld(this); // Spigot - No null worlds
                     this.tileEntityListPending.add(tileentity);
                 } else {
                     this.getChunkAtWorldCoords(blockposition).setTileEntity(blockposition, tileentity);